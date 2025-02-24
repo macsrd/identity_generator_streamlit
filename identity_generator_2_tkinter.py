7fe61231d796d98787eb171d09e9ef975bcedf8e
macsrd <67841350+macsrd@users.noreply.github.com>
2025-02-24 19:03:34 +0100

    update

--- identity_generator_2_tkinter.py
+++ identity_generator_2_tkinter.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import random
-import tkinter as tk
-from tkinter import ttk
+""" import tkinter as tk
+from tkinter import ttk """
 from random_pesel import RandomPESEL
 
 
