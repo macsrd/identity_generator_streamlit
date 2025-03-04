35839f4e11cd0a2f072730ac85e409ef80f510b6
macsrd <67841350+macsrd@users.noreply.github.com>
2025-03-04 13:31:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
 import streamlit as st
-from identity_generator_2_tkinter import generate_identity  # Import your function
+from identity_generator_2_tkinter import generate_identity
 from st_copy_to_clipboard import st_copy_to_clipboard
 
-# Custom CSS for styling
+st.title("Polish Identity Generator")
+
+# Custom CSS to reduce spacing between elements
 st.markdown(
     """
     <style>
-    .main {
-        background-color: #f0f0f0;
-    }
-    .stButton>button {
-        background-color: #4CAF50;
-        color: white;
-        width: 100%;
-        text-align: left;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-    .identity-info {
-        font-size: 18px;
-        font-weight: bold;
-        color: #333;
-        margin-bottom: 10px;
+    .block-container {
+        margin-bottom: 0rem;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-st.title("Polish Identity Generator")
-
-# User selection
 gender = st.radio("Select Gender:", ["Male", "Female"])
 include_secondname = st.checkbox("Include Secondname")
 
-# Generate identity on button click
 if st.button("Generate Identity"):
     try:
         firstname, secondname, lastname, pesel = generate_identity(gender, include_secondname)
