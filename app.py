a0c3969922591275712d593dec29152616c4b716
macsrd <67841350+macsrd@users.noreply.github.com>
2025-03-04 09:34:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ st.markdown(
 )
 
 st.title("Polish Identity Generator")
+"""
 st.header("Generate a Polish Identity")
 st.subheader("Please select the options below:")
+"""
 
 # User selection
 gender = st.radio("Select Gender:", ["Male", "Female"])
@@ -31,14 +33,11 @@ if st.button("Generate Identity"):
         # firstname, secondname, lastname, pesel, probability = generate_identity(gender, include_secondname)
         firstname, secondname, lastname, pesel = generate_identity(gender, include_secondname)
         
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("**Firstname:**", firstname)
-            if include_secondname and secondname:
-                st.write("**Secondname:**", secondname)
-        with col2:
-            st.write("**Lastname:**", lastname)
-            st.write("**PESEL:**", pesel)
+        st.write("**Firstname:**", firstname)
+        if include_secondname and secondname:
+            st.write("**Secondname:**", secondname)
+        st.write("**Lastname:**", lastname)
+        st.write("**PESEL:**", pesel)
         # st.write("**Probability:**", probability)
     except ValueError as e:
         st.error(f"Error generating identity: {e}")
