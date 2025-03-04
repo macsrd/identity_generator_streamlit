106e573f439276e5c62b60d63a4f300e3302a089
macsrd <67841350+macsrd@users.noreply.github.com>
2025-03-04 10:32:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if st.button("Generate Identity"):
         
         st.markdown(f"<div class='identity-info'>Firstname: {firstname}</div>", unsafe_allow_html=True)
         if include_secondname and secondname:
-            st.markdown(f"<div class='identity-info'>Secondname: {secondname}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div class='identity-info'>Secondname: </b>{secondname}</div>", unsafe_allow_html=True)
         st.markdown(f"<div class='identity-info'>Lastname: {lastname}</div>", unsafe_allow_html=True)
         st.markdown(f"<div class='identity-info'>PESEL: {pesel}</div>", unsafe_allow_html=True)
         # st.markdown(f"<div class='identity-info'>**Probability:** {probability}</div>", unsafe_allow_html=True)
