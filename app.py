47ebb8a741a5c5370473dab618ca06d93ee0024d
macsrd <67841350+macsrd@users.noreply.github.com>
2025-03-05 00:00:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ import streamlit as st
 from identity_generator_2_tkinter import generate_identity
 from st_copy_to_clipboard import st_copy_to_clipboard
 
-st.title("Polish Identity Generator")
+# Custom CSS to reduce spacing
+st.markdown("""
+<style>
+div.stMarkdown {
+    margin-bottom: 0rem;
+    padding-bottom: 0rem;
+}
+div.row-widget.stButton {
+    margin-top: 0rem;
+    padding-top: 0rem;
+}
+</style>
+""", unsafe_allow_html=True)
 
-# Custom CSS to reduce spacing between elements
-st.markdown(
-    """
-    <style>
-    .block-container {
-        margin-bottom: 0rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.title("Polish Identity Generator")
 
 gender = st.radio("Select Gender:", ["Male", "Female"])
 include_secondname = st.checkbox("Include Secondname")
@@ -34,7 +36,12 @@ if st.button("Generate Identity"):
 
 if "identity" in st.session_state:
     for key, value in st.session_state.identity:
-        st.markdown(f"**{key}:**")
+        st.markdown(f"""
+        <div style="margin-bottom: 0px; padding-bottom: 0px;">
+            <strong>{key}:</strong><br>
+        </div>
+        """, unsafe_allow_html=True)
+        # The copy button will appear immediately below with minimal spacing
         st_copy_to_clipboard(value, value)
 
 st.info("Click on the values to copy them to your clipboard.")
